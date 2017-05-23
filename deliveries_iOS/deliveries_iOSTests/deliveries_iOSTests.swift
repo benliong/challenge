fcf812fc9467802dd8c5cfadf8de8b341bc3b63f
XCodeClub <xcodeclub@XCodeClubs-Mac-6.local>
2017-05-23 05:42:18 -0400

    initial commit for deliveries iOS Project

---
+++ deliveries_iOS/deliveries_iOSTests/deliveries_iOSTests.swift
@@ -0,0 +1,36 @@
+//
+//  deliveries_iOSTests.swift
+//  deliveries_iOSTests
+//
+//  Created by XCodeClub on 2017-05-23.
+//  Copyright © 2017 Ben Liong. All rights reserved.
+//
+
+import XCTest
+@testable import deliveries_iOS
+
+class deliveries_iOSTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
