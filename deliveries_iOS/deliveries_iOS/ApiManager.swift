6feb199c056bf6f05fc94e3d31be74e3b0f46c28
XCodeClub <xcodeclub@XCodeClubs-Mac-6.local>
2017-05-23 13:26:43 -0400

    chore: add Model and API support for Deliveries

---
+++ deliveries_iOS/deliveries_iOS/ApiManager.swift
@@ -0,0 +1,54 @@
+//
+//  ApiManager.swift
+//  deliveries_iOS
+//
+//  Created by XCodeClub on 2017-05-23.
+//  Copyright © 2017 Ben Liong. All rights reserved.
+//
+
+import UIKit
+import Alamofire
+import ObjectMapper
+import SwiftyJSON
+
+enum ApiError: Error {
+    case unknownError
+    case connectionError
+    case invalidCredentials
+    case invalidRequest
+    case notFound
+    case invalidResponse
+    case serverError
+    case serverUnavailable
+    case timeOut
+    case unsuppotedURL
+}
+
+typealias CompletionClosure = (AnyObject?, Error?) -> Void
+//typealias ArrayCompletionClosure = ([AnyObject], city:City?, ErrorType?) -> Void
+typealias PaginatedArrayCompletionClosure = (_ objects:[Any]?, _ next:Int?, _ error:Error?) -> Void
+
+
+
+class ApiManager: NSObject {
+    static let shared = ApiManager()
+    var baseURLString = "http://192.168.1.100:8080/"
+    func getDeliveries(atOffset offset:Int = 0, completionHandler:@escaping PaginatedArrayCompletionClosure) {
+        let urlString = baseURLString.appending("deliveries")
+        let parameters = ["offset" : offset]
+        Alamofire.request(urlString, method: .get, parameters: parameters, encoding: URLEncoding.default).validate().responseJSON { (response) in
+            switch response.result {
+            case .failure(let error):
+                completionHandler(nil, nil, error)
+            case .success(let value):
+                let json = JSON(value)
+                guard   let jsonString = json.rawString(),
+                        let deliveries:Array<Delivery> = Mapper<Delivery>().mapArray(JSONString: jsonString) else {
+                    completionHandler(nil, nil, ApiError.invalidResponse)
+                    return
+                }
+                completionHandler(deliveries, offset + deliveries.count, nil)
+            }
+        }
+    }
+}
---
+++ deliveries_iOS/deliveries_iOS/Models/Delivery.swift
@@ -0,0 +1,64 @@
+//
+//  Delivery.swift
+//  deliveries_iOS
+//
+//  Created by XCodeClub on 2017-05-23.
+//  Copyright © 2017 Ben Liong. All rights reserved.
+//
+
+import UIKit
+import MapKit
+import ObjectMapper
+
+//{
+//    "description": "Deliver documents to Andrio",
+//    "imageUrl": "http://placekitten.com.s3.amazonaws.com/homepage-samples/200/287.jpg",
+//    "location": {
+//        "lat": 22.336093,
+//        "lng": 114.155288,
+//        "address": "Cheung Sha Wan"
+//    }
+//}
+
+class Location:Mappable {
+    var latitude:CLLocationDegrees?
+    var longitude:CLLocationDegrees?
+    var address:String?
+    
+    required init?(map: Map) {
+        
+    }
+    
+    func mapping(map: Map) {
+        latitude    <- map["lat"]
+        longitude   <- map["lng"]
+        address     <- map["address"]
+    }
+}
+
+class Delivery: Mappable {
+    var details:String?
+    var _imageURLString:String?
+    var imageURL:URL? {
+        get {
+            guard let imageURLString = _imageURLString else {
+                return nil
+            }
+            return URL(string: imageURLString)
+        }
+        set {
+            _imageURLString = newValue?.absoluteString
+        }
+    }
+    var location:Location?
+    
+    required init?(map: Map) {
+        
+    }
+    
+    func mapping(map: Map) {
+        details                 <- map["description"]
+        _imageURLString         <- map["imageUrl"]
+        location                <- map["location"]
+    }
+}
